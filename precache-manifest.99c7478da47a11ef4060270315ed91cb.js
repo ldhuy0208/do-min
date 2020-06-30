6ef9ad32b21f58b81e4181d0297e21528e41271f
unknown <ldhuy0208@gmail.com>
2020-07-01 00:52:58 +0700

    Updates

---
+++ precache-manifest.99c7478da47a11ef4060270315ed91cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a54341ae6f02e28663de114cd469404",
+    "url": "/ldhuy0208/do-min/index.html"
+  },
+  {
+    "revision": "9ed3059b052aa711880d",
+    "url": "/ldhuy0208/do-min/static/css/main.fe4ed18f.chunk.css"
+  },
+  {
+    "revision": "fff1215da8154144ee3e",
+    "url": "/ldhuy0208/do-min/static/js/2.e47c0810.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ldhuy0208/do-min/static/js/2.e47c0810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed3059b052aa711880d",
+    "url": "/ldhuy0208/do-min/static/js/main.32f182a0.chunk.js"
+  },
+  {
+    "revision": "8f307202c418ae8cdb2c",
+    "url": "/ldhuy0208/do-min/static/js/runtime-main.225cbcf6.js"
+  }
+]);
